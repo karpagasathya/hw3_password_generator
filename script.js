0268e40fc706b39b0d200650799358571f1a2ae7
sathya <karpagasathya@gmail.com>
2020-04-22 01:55:40 -0500

    password length validation

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
+var number = "01234567890";
 var specialCharacter = "~!@#$%^&*()_+=";
-
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var copy = document.querySelector("#copy");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+  if (!password) {
+    password = "";
+  }
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -24,42 +25,46 @@ generateBtn.addEventListener("click", writePassword);
 copy.addEventListener("click", copyPassword);
 
 function generatePassword() {
+
+  let passwordLength = parseInt(
+    prompt(
+      "how many characters would you like for your password? choose between 8 and 128"
+    )
+  );
+  console.log(passwordLength);
+  if (!passwordLength) {
+    alert("Password length should be Numeric");
     
-  var passwordLength= getPasswordLength();
-  let isUpperCase = confirm("Do you want Uppercase letters");
-  let isLowerCase = confirm("Do you want Lowercase letters");
-  let isNumber = confirm("Do you want Numbers");
-  let isSpecialCharacter = confirm("Do you want Special Charaters");
-
-  let characterSet = "";
-  if (isUpperCase || isLowerCase || isNumber || isSpecialCharacter) {
-    if (isUpperCase) characterSet += upperCase;
-    if (isLowerCase) characterSet += lowerCase;
-    if (isNumber) characterSet += number;
-    if (isSpecialCharacter) characterSet += specialCharacter;
   } else {
-    alert("You have to choose atleast one charater type");
-  }
-  let finalPassword = "";
-    for (var i = 0; i < passwordLength; i++) {
-        finalPassword += characterSet.charAt(Math.floor(Math.random() * characterSet.length))
-    }
-    return finalPassword;
-}
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("You must choose between 8 and 128");
+    } else {
+      let isUpperCase = confirm("Do you want Uppercase letters");
+      let isLowerCase = confirm("Do you want Lowercase letters");
+      let isNumber = confirm("Do you want Numbers");
+      let isSpecialCharacter = confirm("Do you want Special Characters");
 
-// password length function
-function getPasswordLength() {
-   let passwordLength = prompt("length of the password");
-   console.log(passwordLength);
-   if (!passwordLength) {
-     alert("provide password length");
-     passwordLength = prompt("length of the password");
-   }
-   if (passwordLength < 8 || passwordLength > 128) {
-     alert("provide password length between 8 and 128");
-     passwordLength = prompt("length of the password");
-    } 
-    return passwordLength;
+      let characterSet = "";
+      if (isUpperCase || isLowerCase || isNumber || isSpecialCharacter) {
+        if (isUpperCase) characterSet += upperCase;
+        if (isLowerCase) characterSet += lowerCase;
+        if (isNumber) characterSet += number;
+        if (isSpecialCharacter) characterSet += specialCharacter;
+      } else {
+        alert("You have to choose atleast one character type");
+      }
+
+      let finalPassword = "";
+      for (var i = 0; i < passwordLength; i++) {
+        finalPassword += characterSet.charAt(
+          Math.floor(Math.random() * characterSet.length)
+        );
+      }
+
+      return finalPassword;
+    }
+  }
+
 }
 
 // copy to clipboard function
@@ -68,4 +73,3 @@ function copyPassword() {
   document.execCommand("Copy");
   alert("Password copied to clipboard!");
 }
-  
